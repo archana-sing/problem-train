b53baee1171d1e3b9572cf7092cef1ef2e430f2f
archanasingh1992 <archanasingh@mplgaming.com>
2025-08-22 23:34:59 +0530

    solved two sorted arrays

---
+++ array/merge-two-sorted-array.js
@@ -0,0 +1,20 @@
+// Problem statement
+// merge two sorted arrays ([3,5,7], [1,2,4,6,8]) => [1,2,3,4,5,6,7,8]
+
+function mergeSortedArrays(arr1, arr2) {
+  const mergedArray = [];
+  let i = 0, j = 0;
+  while(i < arr1.length && j < arr2.length) {
+    if (arr1[i] < arr2[j]) {
+      mergedArray.push(arr1[i++]);
+    } else {
+      mergedArray.push(arr2[j++]);
+    }
+  }
+
+  while(i < arr1.length) mergedArray.push(arr1[i++]);
+  while(j < arr2.length) mergedArray.push(arr2[j++]);
+  return mergedArray;
+}
+
+console.log(mergeSortedArrays([2,3,4,5,7], [1,2,4,6,8]))
